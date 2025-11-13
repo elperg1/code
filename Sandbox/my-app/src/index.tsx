e30cffa311c41aa8bc6e675d788794a0b7c1f58e
elliotp <elliot.pergl@gmail.com>
2025-11-13 14:54:28 -0500

    working React app

--- Sandbox/my-app/src/index.tsx
+++ Sandbox/my-app/src/index.tsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@fontsource/roboto"; // Material UI font
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
